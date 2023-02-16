beb73aab42b97b085bf6f06737322b8c06bf1982
GerardPiquerArrebola <97737336+GerardPiquerArrebola@users.noreply.github.com>
2023-02-16 17:15:48 +0100

    modif

--- PRJ_P1_W23_STU/src/p01_CompareAndSet/CompareAndSetBoard.java
+++ PRJ_P1_W23_STU/src/p01_CompareAndSet/CompareAndSetBoard.java
@@ -11,26 +11,48 @@ public class CompareAndSetBoard extends AbstractBoard {
 	/* Add all the boolean attributes you need 
 	 * (no AtomicBoolean)
 	 */
+	private boolean canPaper = true;
+	private boolean canRock = true;
+	private boolean canScissors = true;
+	private boolean canJudge = false;
 	
+	private volatile int count = 0;
 	
 	
 	public void letMeAccessForPaper(int id) {
 		/* COMPLETE */
+		while (!boardBusy.compareAndSet(canPaper, false))
+			Thread.yield();
+		count++;
 	}
 	
 	public void letMeAccessForRock(int id) {
 		/* COMPLETE */
+		while (!boardBusy.compareAndSet(canRock, false))
+			Thread.yield();
+		count++;
 	}
 	
 	public void letMeAccessForScissors(int id) {
 		/* COMPLETE */
+		while (!boardBusy.compareAndSet(canScissors, false))
+			Thread.yield();
+		count++;
 	}
 	
 	public void letMeAccessForJudge (int id) {
 		/* COMPLETE */
+		while (!boardBusy.compareAndSet(canJudge, false) && count < 2)
+			Thread.yield();
 	}
 	
 	public void accessDone () {
 		/* COMPLETE */
+		if (count == 2) {
+			canJudge = true;
+			canPaper = false;
+			canRock = false;
+			canScissors = false;
+		}
 	}
 }
